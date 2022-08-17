636c44168ddacf99e65017d2e7e137df9671ec54
Brian Thomas <brianthomas571@gmail.com>
2022-08-16 19:55:02 -0500

    footer UI updates

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { footerList1, footerList2, footerList3 } from '../utils/constants'
+
+const List = ({footerList, mt}: { footerList: string[], mt: boolean}) => (
+  <div className={`flex flex-wrap gap-2 ${mt && 'mt-5'}`}>
+      {
+        footerList.map((item, index) => (
+          <p key={item} className="text-gray-400 text-sm hover:underline cursor-pointer">{item}</p>
+        ))
+      }
+      </div>
+)
 
 const Footer = () => {
+  const date = new Date().getFullYear()
   return (
-    <div>Footer</div>
+    <div className='mt-6 hidden xl:block '>
+      <List footerList={footerList1} mt={false}/>
+      <List footerList={footerList2} mt/>
+      <List footerList={footerList3} mt/>
+      <p className='text-gray-400 text-sm mt-5'>{date} TikTok</p>
+    </div>
   )
 }
 
